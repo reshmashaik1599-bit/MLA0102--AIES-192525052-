f855607b0fd18ac653c32aefb3a8c35cca3bfd2e
reshmashaik1599-bit <reshmashaik1599@gmail.com>
2026-08-04 11:32:51 +0530

    Add files via upload

---
+++ monkey banana.py
@@ -0,0 +1,16 @@
+def monkey_banana():
+    print("Initial State:")
+    print("Monkey is on the floor.")
+    print("Banana is hanging from the ceiling.")
+    print("Box is in the room.\n")
+
+    print("Steps:")
+    print("1. Monkey moves to the box.")
+    print("2. Monkey pushes the box under the banana.")
+    print("3. Monkey climbs onto the box.")
+    print("4. Monkey reaches the banana.")
+    print("5. Monkey grabs the banana.")
+
+    print("\nGoal Achieved: Monkey got the banana!")
+
+monkey_banana()
